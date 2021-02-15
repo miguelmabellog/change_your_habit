d17e21b9bfbef4598853b9d6f8aed4ff3a90be4a
Miguel Bello <miguel.angel.bello.garcia123@gmail.com>
2021-02-14 21:22:13 -0500

    date work not well

--- app/src/main/java/com/example/changeyourhabit/week/WeekFragment.kt
+++ app/src/main/java/com/example/changeyourhabit/week/WeekFragment.kt
@@ -1,21 +1,17 @@
 package com.example.changeyourhabit.week
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.changeyourhabit.R
-import com.example.changeyourhabit.addNew.AddNewViewModel
-import com.example.changeyourhabit.addNew.AddNewViewModelFactory
 import com.example.changeyourhabit.database.PointDatabase
-import com.example.changeyourhabit.databinding.AddNewFragmentBinding
 import com.example.changeyourhabit.databinding.WeekFragmentBinding
-import com.example.changeyourhabit.databinding.WeekFragmentBindingImpl
+import java.util.*
 
 class WeekFragment : Fragment() {
 
@@ -53,6 +49,9 @@ class WeekFragment : Fragment() {
 
     private fun onDateSelected(day:Int, month:Int, year:Int) {
         Log.i("WeekDialogoFragment", "$day , $month, $year")
+        val cal: Calendar = Calendar.getInstance()
+        viewModel.givedate(day,month,year)
+
     }
 
 
--- app/src/main/java/com/example/changeyourhabit/week/WeekViewModel.kt
+++ app/src/main/java/com/example/changeyourhabit/week/WeekViewModel.kt
@@ -1,10 +1,10 @@
 package com.example.changeyourhabit.week
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.*
 import com.example.changeyourhabit.database.PointDao
 import com.example.changeyourhabit.database.PointDate
-import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -12,62 +12,173 @@ class WeekViewModel (
     val database: PointDao,
     application: Application
 ) : AndroidViewModel(application){
-    private val allPoints=database.getPoints()
     private var numberOfDay:Int?=null
-    private var dayOfMonth:Int?=null
     private val sdf = SimpleDateFormat("dd/M/yyyy")
-    private val currentDate = sdf.format(Date())
+    private val today=Date()
+    var currentDate: String = sdf.format(today)
+    private val searchDate=MutableLiveData<String>()
 
-    val mondayPoints: LiveData<String> = liveData{
-            val data= putNumberForDay(0)?.numberOfPoints?.toString()
-            emit(data?:"0")
+    val mondayPoints=searchDate.switchMap {
+        liveData {
+            var data= putNumberForDay(0,it)?.numberOfPoints?.toString()?:"0"
+            emit(data)
+        }
     }
 
-    val tuesdayPoints: LiveData<String> = liveData{
-        val data= putNumberForDay(1)?.numberOfPoints?.toString()
-        emit(data?:"0")
+    val tuesdayPoints=searchDate.switchMap {
+        liveData {
+            var data= putNumberForDay(1,it)?.numberOfPoints?.toString()?:"0"
+            emit(data)
+        }
     }
 
-    val wednesdayPoints: LiveData<String> = liveData{
-        val data= putNumberForDay(2)?.numberOfPoints?.toString()
-        emit(data?:"0")
+    val wednesdayPoints=searchDate.switchMap {
+        liveData {
+            var data= putNumberForDay(2,it)?.numberOfPoints?.toString()?:"0"
+            emit(data)
+        }
     }
 
-    val thursdayPoints: LiveData<String> = liveData{
-        val data= putNumberForDay(3)?.numberOfPoints?.toString()
-        emit(data?:"0")
+    val thursdayPoints=searchDate.switchMap {
+        liveData {
+            var data= putNumberForDay(3,it)?.numberOfPoints?.toString()?:"0"
+            emit(data)
+        }
     }
 
-    val fridayPoints: LiveData<String> = liveData{
-            val data= putNumberForDay(4)?.numberOfPoints?.toString()
-            emit(data?:"0")
+    val fridayPoints=searchDate.switchMap {
+        liveData {
+            var data= putNumberForDay(4,it)?.numberOfPoints?.toString()?:"0"
+            emit(data)
+        }
     }
 
-    val saturdayPoints: LiveData<String> = liveData{
-        val data= putNumberForDay(5)?.numberOfPoints?.toString()
-        emit(data?:"0")
+
+    val saturdayPoints=searchDate.switchMap {
+        liveData {
+            var data= putNumberForDay(5,it)?.numberOfPoints?.toString()?:"0"
+            emit(data)
+        }
     }
 
-    val sundayPoints: LiveData<String> = liveData{
-        val data= putNumberForDay(6)?.numberOfPoints?.toString()
-        emit(data?:"0")
+
+    val sundayPoints=searchDate.switchMap {
+        liveData {
+            var data= putNumberForDay(6,it)?.numberOfPoints?.toString()?:"0"
+            emit(data)
+        }
     }
+
+    private val dayformat=SimpleDateFormat("EEEE")
+    private var dayOfWeekSearch=dayformat.format(Date())
+
+    private var calendar = Calendar.getInstance()
+    private var daySearch = calendar[Calendar.DAY_OF_MONTH]
+    private var monthSearch=calendar[Calendar.MONTH]+1
+    private var yearSearch=calendar[Calendar.YEAR]
+
+
+
+
+    fun givedate(day:Int,month:Int,year:Int){
+        val dateUpdate="$day/${month+1}/$year"
+
+
+        calendar.set(Calendar.DAY_OF_MONTH,day)
+        calendar.set(Calendar.MONTH,month)
+        calendar.set(Calendar.YEAR,year)
+
+
+        dayOfWeekSearch=dayformat.format(calendar.time)
+
+        searchDate.value=dateUpdate
+        daySearch=day
+        monthSearch=month+1
+        yearSearch=year
+
+        refreshDateText(daySearch,monthSearch,yearSearch,dayOfWeekSearch,calendar)
+    }
+
+    init {
+        searchDate.value=currentDate
+        refreshDateText(daySearch,monthSearch,yearSearch,dayOfWeekSearch,calendar)
+    }
+
+    private var _mondayDate=MutableLiveData<String>()
+    val  mondayDate: LiveData<String>
+            get()=_mondayDate
+
+    private var _tuesdayDate=MutableLiveData<String>()
+    val  tuesdayDate: LiveData<String>
+        get()=_tuesdayDate
+
+    private var _wednesdayDate=MutableLiveData<String>()
+    val  wednesdayDate: LiveData<String>
+        get()=_wednesdayDate
+
+    private var _thursdayDate=MutableLiveData<String>()
+    val  thursdayDate: LiveData<String>
+        get()=_thursdayDate
+
+    private var _fridayDate=MutableLiveData<String>()
+    val  fridayDate: LiveData<String>
+        get()=_fridayDate
+
+    private var _saturdayDate=MutableLiveData<String>()
+    val  saturdayDate: LiveData<String>
+        get()=_saturdayDate
+
+    private var _sundayDate=MutableLiveData<String>()
+    val  sundayDate: LiveData<String>
+        get()=_sundayDate
+
+    fun refreshDateText(day:Int, month: Int, year: Int, dayOfWeek: String, calendar: Calendar){
+
+        when (dayOfWeek){
+            "Monday"->{ _mondayDate.value=sdf.format(calendar.time)
+                _tuesdayDate.value=sdf.format(addCalendar(1,calendar).time)
+                _wednesdayDate.value=sdf.format(addCalendar(2,calendar).time)
+                _thursdayDate.value=sdf.format(addCalendar(3,calendar).time)
+                _fridayDate.value=sdf.format(addCalendar(4,calendar).time)
+                _saturdayDate.value=sdf.format(addCalendar(4,calendar).time)
+                _sundayDate.value=sdf.format(addCalendar(5,calendar).time)
+
+                        }
+
+        }
+
+    }
+
+    fun addCalendar(add:Int, calendar: Calendar):Calendar{
+        calendar.add(Calendar.DATE, +(add))
+        return calendar
+    }
+
+
+
+
+
 
     private suspend  fun putPoints(tag: Int): PointDate? {
         var calendar: Calendar
         var key_search: String
-        var result_search: PointDate?
+        var result_search: PointDate?=null
 
-        calendar = Calendar.getInstance()
-        calendar.add(Calendar.DATE, -(numberOfDay!!-tag))
-        key_search=sdf.format(calendar.time)
-        result_search=database.getToday(key_search)
+        if(numberOfDay != null){
+            calendar = Calendar.getInstance()
+            calendar.add(Calendar.DATE, -(numberOfDay!! -tag))
+            key_search=sdf.format(calendar.time)
+            result_search=database.getToday(key_search)
+        }
+
 
         return result_search
     }
 
-    private suspend fun putNumberForDay(tag:Int): PointDate? {
-            var todayDate=database.getToday(currentDate)
+    private suspend fun putNumberForDay(tag:Int, searchDate:String): PointDate? {
+        Log.i("PutNumberforDay",searchDate)
+            var todayDate=database.getToday(searchDate)
+
             numberOfDay = when (todayDate?.dayOfWeek){
                 "Monday"-> 0
                 "Tuesday"-> 1
